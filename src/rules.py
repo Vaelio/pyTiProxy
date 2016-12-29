f93c639016016aa48e95d5d9356379923a22bb18
Vaelio <valentin.eudeline@gmail.com>
2016-12-29 13:45:30 +0100

    Updated rules.py

--- src/rules.py
+++ src/rules.py
@@ -1,41 +1,42 @@
 #!/bin/env python
 # -*- coding:utf-8 -*-
 
+from re import findall
 
-hacker_agent = {'SQLMAP', 'USERAGENT', 'NIKTO', 'VEGA', 'BLACKSUN', 'NESSUS'}
+hacker_agent = {b'SQLMAP', b'USERAGENT', b'NIKTO', b'VEGA', b'BLACKSUN', b'NESSUS'}
 hacker_data = [b'\'', b'SELECT', b'UNION', b'AND', b'LIKE', b' ', b'%20', b'%2520',
                b'DROP', b'LOAD', b'FILE', b'SCRIPT', b'DOCUMENT', b'COOKIE']
 hacker_referer = {'%3D', '%27', "'", "%"}
 
 def catch_hackers(client_infos, addr, sock_client, fdclient, msg, detect):
     try:
-        finder_agent = re.findall('\s*\(?(.+?)[/\s][\d.]+', client_infos['user_agent'][0])
+        finder_agent = re.findall(b'\s*\(?(.+?)[/\s][\d.]+', client_infos[b'user_agent'][0])
         if addr[0] not in hackers:
-            hackers[addr[0]] = {'count': 0}
+            hackers[addr[0]] = {b'count': 0}
         for string in finder_agent:
             if string in hacker_agent:
-                hackers[addr[0]]['message'] = msg
+                hackers[addr[0]][b'message'] = msg
                 generate_404(fdclient)
                 sock_client.close()
                 detect = True
-        for item in client_infos['referer']:
+        for item in client_infos[b'referer']:
             if item in hacker_referer:
-                hackers[addr[0]]['message'] = msg
+                hackers[addr[0]][b'message'] = msg
                 generate_404(fdclient)
                 sock_client.close()
                 detect = True
-        if client_infos['data'] is not None:
+        if client_infos[b'data'] is not None:
             for item in hacker_data:
-                if item in client_infos['data']:
-                    hackers[addr[0]]['message'] = msg
+                if item in client_infos[b'data']:
+                    hackers[addr[0]][b'message'] = msg
                     generate_404(fdclient)
                     sock_client.close()
                     detect = True
-    except IndexError, e:
-        print e
+    except IndexError as  e:
+        print(e)
         sock_client.close()
         generate_404(fdclient)
         detect = True
-return detect
+    return detect
 
 
